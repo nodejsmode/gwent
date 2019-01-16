fb78a86fb6fd281fac54634ec3a133ff8c27e55e
nodejsmode <44375868+nodejsmode@users.noreply.github.com>
2019-01-16 20:54:27 +0300

    Github is coming

--- gwent.js
+++ gwent.js
@@ -51,6 +51,57 @@ var myHand = new Vue({
 
 
 
+var oppHand = new Vue({
+    el: '#opp-hand',
+    data: {
+        // здесь хранятся только картинки, сам объект строится в computed и именно он используется в v-for
+        cards: [
+            {id: 0, url: 'img/back.png', bias: 0, isChoosed: false},
+            {id: 1, url: 'img/back.png', bias: 0, isChoosed: false},
+            {id: 2, url: 'img/back.png', bias: 0, isChoosed: false}
+        ],
+        whatChoosed: undefined
+    },
+    watch: {
+        // следим за изменением состава массива cards
+        cards: function(newCards, oldCards) {
+
+            for (var i = 0; i < newCards.length; i++) {
+
+                // вычислим на сколько сдвинуть карты влево, если их много
+                var bias;
+                if (i === 0 || newCards.length <= MYHANDMAXCARDS) { // если это первая карта или карт меньше 7, то всем 0
+                    bias = 0
+                } else {   // если это не первая карта и карт больше 7, то со 2-й вычисляем
+                    bias = Math.floor(MYHANDDIVWIDTH / (newCards.length) - MYHANDDIVWIDTH / MYHANDMAXCARDS);
+                }
+
+                oldCards[i].bias = bias + 'px';
+            }
+        }
+    },
+
+    methods: {
+        choose: function (newId) {
+            console.log(newId, this.whatChoosed);
+
+            if (this.whatChoosed !== undefined) {
+                this.cards[this.whatChoosed].isChoosed = false
+            }
+
+            this.cards[newId].isChoosed = true;
+            this.whatChoosed = newId;
+        }
+    }
+
+});
+
+
+
+
+
+
+
 /*
 $(window).ready(function () {
     var a = {
